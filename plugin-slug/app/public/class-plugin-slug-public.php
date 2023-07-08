3cd5161b06cf5fea6b3c6d950f8fce539efcd806
Jay Patel <84016197+Hackkzy@users.noreply.github.com>
2023-07-08 15:15:05 +0530

    Update class-plugin-slug-public.php

--- plugin-slug/app/public/class-plugin-slug-public.php
+++ plugin-slug/app/public/class-plugin-slug-public.php
@@ -25,34 +25,32 @@ if ( ! class_exists( 'Plugin_Slug_Public' ) ) {
 		 */
 		public function __construct() {
 			// Enque custom scripts.
-			add_action( 'admin_enqueue_scripts', array( $this, 'plugin_slug_enqueue_scripts' ) );
+			add_action( 'admin_enqueue_scripts', array( $this, 'plugin_prefix_enqueue_scripts' ) );
 		}
 
 		/**
 		 * Enqueue custom frontend scripts.
 		 */
-		public function plugin_slug_enqueue_scripts() {
-			$plugin_slug_js  = 'plugin-slug-public.min.js';
-			$plugin_slug_css = 'plugin-slug-public.min.css';
+		public function plugin_prefix_enqueue_scripts() {
+			$plugin_asset  = 'plugin-slug-public';
 
-			if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
-				// If Script Debug enabled then include non minified files.
-				$plugin_slug_js = 'plugin-slug-public.js';
-				$plugin_slug_js = 'plugin-slug-public.css';
+			if ( ! defined( 'SCRIPT_DEBUG' ) || ! SCRIPT_DEBUG ) {
+				// If Script debug disabled then include minified files.
+				$plugin_asset .= '.min';
 			}
 
 			// Plugin Related Style and Scripts.
 			wp_enqueue_script(
-				'plugin-slug-admin',
-				trailingslashit( PLUGIN_PREFIX_URL ) . 'app/public/assets/js/' . $plugin_slug_js,
+				'plugin-slug-public',
+				trailingslashit( PLUGIN_PREFIX_URL ) . 'app/admin/assets/js/' . $plugin_asset . '.js',
 				array( 'jquery' ),
 				PLUGIN_PREFIX_VERSION,
 				true
 			);
 
 			wp_enqueue_style(
-				'plugin-slug-admin',
-				trailingslashit( PLUGIN_PREFIX_URL ) . 'app/public/assets/css/' . $plugin_slug_css,
+				'plugin-slug-public',
+				trailingslashit( PLUGIN_PREFIX_URL ) . 'app/admin/assets/css/' . $plugin_asset . '.css',
 				array(),
 				PLUGIN_PREFIX_VERSION
 			);
